c5618738e0b9fcca82e1e079af0a18ee1c28d3aa
Shrina sunil handoo <155298579+Shree-001@users.noreply.github.com>
2025-08-14 18:19:21 +0530

    Time: 4 ms (37.70%) | Memory: 42.9 MB (23.66%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -1,17 +1,19 @@
 import java.util.regex.*;
-
 class Solution {
     public String largestGoodInteger(String num) {
-        Pattern pattern = Pattern.compile("(\\d)\\1\\1");
-        Matcher matcher = pattern.matcher(num);
-
-        String max = "";
-        while (matcher.find()) {
-            String match = matcher.group();
-            if (max.isEmpty() || match.compareTo(max) > 0) {
-                max = match;
-            }
+        
+        Pattern p =Pattern.compile("(\\d)\\1\\1");
+        Matcher m=p.matcher(num);
+        String max="";
+        while(m.find()){// scans the string in O(n)
+        String match=m.group();/// stroes the substring that matches "777"
+        if(max.isEmpty() || match.compareTo(max)>0)
+        {
+            max=match;
+        }
         }
         return max;
+
+
     }
 }
